f9c6f40fcff9c020bfc88755b20418f6547ce434
Bryan <king20050723@gmail.com>
2025-11-16 16:03:10 +0800

    Add todos

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,11 +3,13 @@ from .embedding_service import EmbeddingService
 from .database_service import DatabaseService
 from .llm_service import LLMService
 from .annotating_service import ImageAnnotationService, DetectedObject
+from .airpods_manual_fetcher_service import AirpodsManualFetcher
 
 __all__ = [
     "EmbeddingService",
     "DatabaseService",
     "LLMService",
     "ImageAnnotationService",
-    "DetectedObject"
+    "DetectedObject",
+    "AirpodsManualFetcher"
 ]
